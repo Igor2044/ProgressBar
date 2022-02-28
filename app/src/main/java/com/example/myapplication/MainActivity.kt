49236ca232c536690845c175ab6078140d484816
igor <ip1504@wp.pl>
2022-02-28 16:39:14 +0100

    Why

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+
+
+class MainActivity : AppCompatActivity() {
+    val button = findViewById<Button>(R.id.buttonA)
+    val maxNum = findViewById<EditText>(R.id.maxNumber)
+    val progressB = findViewById<ProgressBar>(R.id.progressB)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            var maxN: Int = maxNum.text.toString().toInt()
+            progressB.max = maxN
+        }
+
+    }
+}
